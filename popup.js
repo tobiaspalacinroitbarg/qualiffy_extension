01244a918d4199a44697f340d50036ae4cf13b5b
tpalacin <78242869+tobiaspalacinroitbarg@users.noreply.github.com>
2024-10-16 15:15:20 -0300

    Add files via upload

--- popup.js
+++ popup.js
@@ -4,7 +4,6 @@ let audioContext;
 let audioSource;
 let audioDestination;
 let mediaStreamDestination;
-
 // Función para iniciar la captura de audio de la pestaña
 function startCapture() {
   chrome.tabCapture.capture({audio: true}, (stream) => {
@@ -12,48 +11,37 @@ function startCapture() {
       console.error('Error al capturar el audio');
       return;
     }
-
     // Crear el contexto de audio
     audioContext = new AudioContext();
     audioSource = audioContext.createMediaStreamSource(stream);
-
     // Crear un destino que permita reproducir el audio en los altavoces
     audioDestination = audioContext.destination;
-
     // Conectar el flujo capturado al destino para que se reproduzca
     audioSource.connect(audioDestination);
-
     // Crear un destino separado para la grabación
     mediaStreamDestination = audioContext.createMediaStreamDestination();
     audioSource.connect(mediaStreamDestination);
-
     // Usar el destino para capturar el audio y grabarlo
     mediaRecorder = new MediaRecorder(mediaStreamDestination.stream, { mimeType: 'audio/webm' });
-
     mediaRecorder.ondataavailable = (event) => {
       if (event.data.size > 0) {
         audioChunks.push(event.data);
       }
     };
-
     mediaRecorder.onstop = () => {
       const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
       audioChunks = [];
-
       convertToWav(audioBlob);
     };
-
     mediaRecorder.start();
   });
 }
-
 // Función para detener la captura
 function stopCapture() {
   if (mediaRecorder) {
     mediaRecorder.stop();
   }
 }
-
 // Convertir el blob a archivo .wav
 function convertToWav(blob) {
   const reader = new FileReader();
@@ -71,7 +59,6 @@ function convertToWav(blob) {
     document.body.removeChild(a);
   };
 }
-
 // Añadir event listeners a los botones
 document.querySelector('.start-btn').addEventListener('click', startCapture);
 document.querySelector('.stop-btn').addEventListener('click', stopCapture);
